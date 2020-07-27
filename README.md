bc5a0d1e8edbf8e8439185030da38df441bb2153
Matt Crump <crumpmj@gmail.com>
2020-07-27 16:55:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Hemisphere Suite: Alternate Alternate Firmware for Ornament and Crime
+===
+
+![My image](https://farm1.staticflickr.com/676/20090774694_b56e557693_b.jpg)
+
+
+### firmware:
+
+Hemisphere Suite is an open-source project by Jason Justian (aka chysn)
+
+ornament**s** & crime**s** is a collaborative project by Patrick Dowling (aka pld), mxmxmx and Tim Churches (aka bennelong.bicyclist) (though mostly by pld and bennelong.bicyclist). it **(considerably) extends** the original firmware for the o_C / ASR eurorack module, designed by mxmxmx.
+
+### hardware:
+
+eurorack / teensy 3.2 DAC8565 quad 16bit CV module w/ OLED display
+
+14HP, depth ~ 25mm
+
+build guide: http://ornament-and-cri.me/
+
---
+++ software/o_c_REV/OC_options.h
@@ -0,0 +1,28 @@
+/*
+ *
+ * compile options.
+ *
+ */
+
+#ifndef OC_OPTIONS_H_
+#define OC_OPTIONS_H_
+
+/* ------------ uncomment for Buchla/non-octaval/quartertone support: -------------------------------  */
+//#define BUCHLA_SUPPORT
+/* ------------ uncomment for use with Northernlight cOC program card: ------------------------------  */
+//#define BUCHLA_cOC
+/* ------------ uncomment for use with Northernlight 4U / cOC or 2OC: -------------------------------  */
+//#define BUCHLA_4U
+/* ------------ uncomment for use with Northernlight 2OC on the left side: --------------------------  */
+//#define NORTHERNLIGHT_2OC_LEFTSIDE
+/* ------------ print debug messages to USB serial --------------------------------------------------  */
+//#define PRINT_DEBUG
+/* ------------ flip screen / IO mapping ------------------------------------------------------------  */
+//#define FLIP_180
+/* ------------ invert screen pixels ----------------------------------------------------------------  */
+//#define INVERT_DISPLAY
+/* ------------ use DAC8564 -------------------------------------------------------------------------  */
+//#define DAC8564
+
+#endif
+
